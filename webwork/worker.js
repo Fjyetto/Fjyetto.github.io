1b84c891b42188c02d02190d42738a5d96bbe5a1
Henry Rochet <henry.rochet@orange.fr>
2022-09-27 15:37:30 +0200

    Test

--- webwork/worker.js
+++ webwork/worker.js
@@ -1,7 +1,8 @@
 // worker
 
-import {Vector2} from './Vector2.js';
-
+//import {Vector2} from './Vector2.js';
+importScripts('Vector2.js');
+let Res = 120;
 const Map = [
 "#################",
 "#      #     #  #",
@@ -28,8 +29,15 @@ function ray(origin,angle,max,incr){
 }
 
 self.addEventListener('message' function(e){
-	if (e.data[0]=='M'){
-		
+	if (e.data[0]=='R'){
+		let Dist = ray(e.data[1],e.data[2],e.data[3],e.data[4])[0];
+		let i = e.data[5];
+		let ctx = e.data[6];
+		let dtM = ((i/Res)-.5)**2;
+		let Len = Math.atan(Math.sqrt(1/Dist)*.2)*1000+(dtM*40)/Dist;
+		ctx.fillStyle = 'rgb(10,'+ Math.floor((5.02-Dist)*53) + ','+ Math.floor(Len) +')';
+		ctx.fillRect(i*Size,(360-Len)/2,Size,Len);
+		//postMessage(ray(e.data[1],e.data[2],e.data[3],e.data[4]));
 	}
 	self.close();
 });
