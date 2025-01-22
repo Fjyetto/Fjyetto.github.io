444a346c534ab2b63d9086c9a79d8ec3feb25223
Papatomique <henry.rochet@orange.fr>
2025-01-22 09:03:00 +0100

    hack pt1

---
+++ RETROWORLDE/CheckMobile.js
@@ -0,0 +1,4 @@
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+	if (confirm("Go to mobile version?")==true) window.location.replace("https://papatomique.github.io/m/");
+}
