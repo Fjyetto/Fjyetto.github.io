9ae3e575e00440ea74bf8ced07aa8917eae9e9e6
Papatomique <henry.rochet@orange.fr>
2025-09-26 01:36:30 +0200

    yeah yea

--- CheckMobile.js
+++
@@ -1,4 +0,0 @@
-
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-	if (confirm("Go to mobile version?")==true) window.location.replace("https://papatomique.github.io/m/");
-}
